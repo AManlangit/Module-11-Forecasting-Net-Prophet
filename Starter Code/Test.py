a00f27d58e01cd673115b7327b719484a078f840
AManlangit <142865655+AManlangit@users.noreply.github.com>
2023-11-28 18:44:07 -0800

    Starter files

---
+++ Starter Code/Test.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+
+if 5 >2:
+    print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
---
+++ Starter Code/Test2.py
@@ -0,0 +1,44 @@
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = 'John'
+print(x)
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ["potato", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z)
